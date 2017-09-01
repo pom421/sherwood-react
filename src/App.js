b3b4f2d2da01c5c6a484ce50bbc6e0734d20305a
Po Mauguet <mauguet.po@gmail.com>
2017-09-01 19:34:56 +0200

    création de l'UI statique

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import logo from './img/logo.svg';
+//import logo from './img/logo.svg';
 import './css/App.css';
 
+/*
 class App extends Component {
   render() {
     return (
@@ -16,6 +17,89 @@ class App extends Component {
       </div>
     );
   }
+}
+*/
+
+class App extends Component {
+
+	render() {
+	    return (
+            <div>
+                <Banner></Banner>
+                <CostsUI costs={ this.props.costs }></CostsUI>
+            </div>
+        )
+	}
+}
+
+
+class Banner extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Sherwood</h1>
+            </div>
+        )
+    }
+}
+
+class CostsUI extends Component {
+
+    render() {
+        return (
+            <div>
+                <MonthlyCostsSummary costs={ this.props.costs }></MonthlyCostsSummary>
+                <ListCostRows costs={ this.props.costs }></ListCostRows>
+            </div>
+        )
+    }
+}
+
+class MonthlyCostsSummary extends Component {
+
+    render() {
+
+        var sum = this.props.costs
+            .map(cur => +cur.amount)
+            .reduce((curr, prev) => curr + prev, 0);
+
+
+        return (
+            <div>Frais du mois d'août { sum } €</div>
+        )
+    }
+
+}
+
+class ListCostRows extends Component {
+
+    render() {
+
+        var rows = this.props.costs
+            .map(curr => {
+                return (
+                    <tr key={ curr.id }>
+                        <td>
+                            { curr.date } { curr.amount }€<br/>
+                            { curr.reason }
+                        </td>
+                    </tr>
+                )
+            }
+    	)
+
+
+		return (
+            <div>
+                <h2>Historique des dépenses</h2>
+                <table>
+                    <tbody>{ rows }</tbody>
+                </table>
+            </div>
+        )
+    }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var COSTS = [
+	{ id: 1, date: "13/08/2017", amount: "27", reason:"Restaurant"},
+	{ id: 2, date: "04/08/2017", amount: "23", reason:"Docteur"},
+	{ id: 3, date: "01/07/2017", amount: "400", reason:"Hébergement Dordogne"}
+]
+
+ReactDOM.render(<App costs={COSTS}/>, document.getElementById('root'));
 registerServiceWorker();
