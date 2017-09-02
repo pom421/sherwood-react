62d947116f39e8d26058e4e9f705898fe432a471
Po Mauguet <mauguet.po@gmail.com>
2017-09-02 20:57:33 +0200

    ajout de React router

--- src/App.js
+++ src/App.js
@@ -2,36 +2,65 @@ import React, { Component } from 'react';
 //import logo from './img/logo.svg';
 import './css/App.css';
 
-/*
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-*/
+import {
+	BrowserRouter as Router,
+	Route,
+	Link,
+	Switch
+} from 'react-router-dom';
+
 
 class App extends Component {
 
 	render() {
-	    return (
-            <div>
-                <Banner></Banner>
-                <CostsUI costs={ this.props.costs }></CostsUI>
-            </div>
-        )
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/" render={() => <IndexPage costs={ this.props.costs } />} />
+					<Route path="/detail" component={ DetailPage }></Route>
+				</Switch>
+			</Router>
+		)
 	}
 }
 
+class IndexPage extends Component {
+
+	render() {
+		return (
+			<div>
+				<Banner></Banner>
+				<CostsUI costs={this.props.costs}></CostsUI>
+				<Link to="/detail">Ajouter</Link>
+			</div>
+		)
+	}
+}
+
+class DetailPage extends Component {
+
+	render() {
+		return (
+			<div>
+				<Banner></Banner>
+				<Link to="/">Retour</Link>
+				<DetailCost></DetailCost>
+			</div>
+
+		)
+	}
+}
+
+class DetailCost extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Page de détail</h1>
+            </div>
+        )
+    }
+}
 
 class Banner extends Component {
 
