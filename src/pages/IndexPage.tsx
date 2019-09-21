499742d103979f8d5a5a4e77c902530e4a4b7488
pom421 <mauguet.po@gmail.com>
2019-09-22 01:49:30 +0200

    utilisation de hook pour IndexPage

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 import Banner from "components/Banner"
 import MonthlyCostsSummary from "components/MonthlyCostsSummary"
@@ -10,44 +10,32 @@ interface State {
    costs: Array<Cost>
 }
 
-export default class IndexPage extends Component<any, State> {
-   constructor(props) {
-      super(props)
+export default function IndexPage() {
 
-      this.state = {
-         costs: [] as Array<Cost>
-      }
-   }
+   const [costs, setCosts ] = useState([])
 
-   componentDidMount() {
-      this.updateCosts()
-   }
-
-   updateCosts() {
+   useEffect(() => {
       getAllCosts()
          .then(response => {
             console.log("Success:", response)
-            this.setState({
-               costs: response
-            })
+            setCosts(response)
          })
          .catch(error => console.error("Error:", error))
-   }
+   }, [])
 
-   render() {
-      return (
-         <div>
-            <Banner />
-            <MonthlyCostsSummary costs={this.state.costs} />
-            <ListCostRows costs={this.state.costs} />
-            <div style={{textAlign: "center"}}>
-              <Link to="/detail">
-                <button type="button" className="btn btn-primary btn-lg">
-                    Ajouter
-                </button>
-              </Link>
-            </div>
+   return (
+      <div>
+         <Banner />
+         <MonthlyCostsSummary costs={ costs } />
+         <ListCostRows costs={ costs } />
+         <div style={{textAlign: "center"}}>
+            <Link to="/detail">
+               <button type="button" className="btn btn-primary btn-lg">
+                  Ajouter
+               </button>
+            </Link>
          </div>
-      )
-   }
+      </div>
+   )
+
 }
